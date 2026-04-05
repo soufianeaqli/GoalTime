c5f0be300a6685dd5c791a47fc4f5f9f3c017d2f
Hannibal <aqlisoufiane364@gmail.com>
2026-04-05 23:46:28 +0100

    hebergers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import Login from './Lwst/Login';
 import LoginPrompt from './Lwst/LoginPrompt';
 import TournoiDetail from './Lwst/TournoiDetail';
 import Parametres from './Lwst/Parametres';
-import "./lwst/lwst.css";
+import "./Lwst/lwst.css";
 import * as reservationService from "./services/reservationService";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
